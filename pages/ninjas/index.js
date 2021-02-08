1fdee632b8d48e55ca21d425c55902e318d631c7
Debasish Gracias <d.gracias@clerkswell.com>
2021-02-08 06:25:53 +0000

    added ninja listing functionality

--- pages/ninjas/index.js
+++ pages/ninjas/index.js
@@ -1,10 +1,30 @@
+import styles from '../../styles/Ninjas.module.css'
+import Link from 'next/link'
 
-function index() {
+function index({ ninjas }) {
     return (
         <div>
             <h1>All Ninjas</h1>
+            {ninjas.map(ninja => (
+                <Link href={'/ninjas/'+ninja.id} key={ninja.id}>
+                    <a className={styles.single}>
+                        <h3>{ninja.name}</h3>
+                    </a>
+                </Link>
+            ))}
         </div>
     )
 }
 
 export default index
+
+// This function runs at build time
+//Never runs in the browser
+export const getStaticProps = async () => {
+    const res = await fetch('https://jsonplaceholder.typicode.com/users');
+    const data = await res.json();
+
+    return {
+        props: { ninjas: data }
+    }
+}
